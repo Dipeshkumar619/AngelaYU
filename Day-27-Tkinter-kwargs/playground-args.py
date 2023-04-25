3d2766770b9fb1e4b950a4de17a8df68519a6552
dipeshkumar619 <dipeshkumar619@gmail.com>
2023-04-25 19:54:47 +0530

    Day-27-args-funda

--- Day-27-Tkinter-kwargs/playground-args.py
+++ Day-27-Tkinter-kwargs/playground-args.py
@@ -4,4 +4,12 @@ def add(*args):
         sum+=i
     return sum
 
-print(add(2,3,4,5))
+print(add(2,3,4,5,53,56,78,90))
+
+def multi(*numbers):
+    product=1
+    for i in numbers:
+        product*=i
+    return product
+
+print(multi(2,3,4,5,53,56,78,90))
